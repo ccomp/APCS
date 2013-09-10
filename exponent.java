a1fc2147bb75e5ed55199973f8f660f6354e7835
ccomp <cyron.completo@gmail.com>
2013-09-10 07:40:56 -0600

    Update exponent.java

--- exponent.java
+++ exponent.java
@@ -1,18 +1,18 @@
-import java.util.Scanner;
+// import java.util.Scanner;
 
 public class exponent {
 
 	public static void main(String[] args) {
-		Scanner ex = new Scanner(System.in);
+		// Scanner ex = new Scanner(System.in);
 		integerPower(2);
 
 	}
 
 	public static void integerPower(int x) {
 		double term = x;
-		double n=1.0;
+		double q=1.0;
 
-		double answer = n*x+1;
+		double answer = q*x+1;
 
 		System.out.println("iteration: " + 1);
 		System.out.println("term: " + term);
@@ -24,16 +24,19 @@ public class exponent {
 		double trueanswer = factorial + answer;
 		System.out.println("sum: " + trueanswer);
 
-		for (double i=1; i>1.0E-12; i=i/10) {
-			while (n<=20) {
-				n=2;
-				n++;
-				y++;
-				answer+=term;
-				term = x*i;
-				x++;
-			}
+		for (int q = 2; q <= 20; q++){
 
+		        term *= x/q;
+
+		        sum += term;
+
+		        System.out.println("time: " + q);
+
+		        System.out.println("term: " + term);
+
+		        System.out.println("sum: " + sum);
+
+		    };
 		}
 	}
 	
