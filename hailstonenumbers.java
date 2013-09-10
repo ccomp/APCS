5db9d4d05fb67e61ea3e812a61806ac1869c2dd2
ccomp <cyron.completo@gmail.com>
2013-09-09 19:48:17 -0600

    New hailstone numbers

--- hailstonenumbers.java
+++ hailstonenumbers.java
@@ -1,37 +1,32 @@
-import java.util.Scanner;
-
 public class hailstonenumbers {
-	
+
 
 	public static void main(String[] args) {
-		Scanner ns = new Scanner(System.in);
+		hailstone(12);
 
-		System.out.println("What is your initial number?");
-		int first = ns.nextInt();
+	}
 
+	public static void hailstone(int x){
+		while (x > 1){
 
-		while (int first%2==0 && int first>1) {
-			System.out.print(first);
-			System.out.println("\teven, next value is"+ first+"/2");
-			int first=first/2;
-			return first;
-		}
+		 	if (x%2==0 && x>1) {
 
-		for (int first%2!=0 && int first>1) {
-			System.out.print(first);
-			System.out.println("\todd, next value is" + "3*" + first + " +1");
-			int first=(3*first)+1;
-			return first;
-		}
-
-		if (int first == 1 ) {
-			System.out.print(first);
-			System.out.println("\tstop calculation");
-		}
+	 		System.out.print(x);
+        	System.out.println("\teven, next value is"+ x+"/2");
+		 	x = x/2;
+        	};
 
 
+	 		if (x%2!=0 && x>1) {
 
+			System.out.print(x);
+	 		System.out.println("\todd, next value is" + "3*" + x + " +1");
+			x=(3*x)+1;
 
+		 	};
+	 	}
+    	System.out.print(1);
+        System.out.println("\tstop calculation");
 
 	}
 
